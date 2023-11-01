2d6cc63ca91092ef897c609a4dd40bc0ec2eaee7
HladczukLe <leticia.cassia.hr@gmail.com>
2023-11-01 01:28:39 -0300

    add exercise L

--- lista-2/ultrapassa.c
+++ lista-2/ultrapassa.c
@@ -1,13 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct _node{
-    int data;
-    struct _node *next;
+typedef struct node{
+    int value;
+    struct node *next;
 } Node;
 
-void append(Node **head, int value){
-    Node *newNode = (Node *) calloc(1, sizeof(Node));
-    newNode->data = value;
-    newNode->next = NULL;
+void append(Node **list, int num){
+    Node *aux, *new = malloc(sizeof(Node)); 
+
+    if(new){ 
+        new->value = num;
+        new->next = NULL; 
+
+        //é o primeiro?
+        if(*list ==NULL){ 
+            *list=new;
+        }
+        else{
+            aux =  *list;
+            while(aux->next){ 
+                aux = aux->next;
+            }
+            aux ->next = new;
+        }
+    }
 }
+void appendFirst(Node **list, int num){
+    Node *new = malloc(sizeof(Node));
+
+    if(new){
+        new->value = num;
+        new->next = *list;
+        *list = new;
+    }
+}
+void printLinkedList(Node *node){
+    while(node){
+        printf("%d\n", node->value);
+        node = node->next;
+    }
+}
+
+void printSumExceedingLimit(Node *node, int sumLimit) {
+    Node *end = node;
+    Node *list2 = NULL;
+    int sum = 0;
+
+    while (node) {
+        sum += node->value;
+
+        if (sum > sumLimit) {
+            end = node;
+            appendFirst(&list2, end->value);
+            sum = 0;
+        }
+        
+
+        node = node->next;
+    }
+    printLinkedList(list2);
+}
+
+
+
+void freeList(Node *node) {
+    while (node) {
+        Node *temp = node;
+        node = node->next;
+        free(temp);
+    }
+}
+
+ int main(){
+    int value, sumLimit;
+    Node *list = NULL;
+
+    do {
+        scanf("%d", &value);
+        append(&list, value);
+    } while (value != 0);
+
+    scanf("%d", &sumLimit);
+
+    printSumExceedingLimit(list, sumLimit);
+
+    freeList(list);
+
+    
+    return 0;
+ }
