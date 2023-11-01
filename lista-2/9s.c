c4cfcb7088245e00401c8e6be391ac5384fc2e0f
HladczukLe <leticia.cassia.hr@gmail.com>
2023-11-01 00:32:25 -0300

    fix 9s.c

--- lista-2/9s.c
+++ lista-2/9s.c
@@ -1,44 +1,42 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-int sumOfDigits(char *num) {
+int degree9(char *num, int degree) {
+    if (strlen(num) == 1 && num[0] == '9') {
+        return degree;
+    } else if (strlen(num) == 1) {
+        return -1;
+    }
+
     int sum = 0;
     for (int i = 0; num[i]; i++) {
         sum += num[i] - '0';
     }
-    return sum;
-}
-
-int degree9(char *num) {
-    if (strlen(num) == 1 && num[0] == '9') {
-        return 1;
-    } else if (strlen(num) == 1) {
-        return 0;
-    }
-
-    int sum = sumOfDigits(num);
     char sumStr[5];
     sprintf(sumStr, "%d", sum);
 
-    return 1 + degree9(sumStr);
+    if (sum != 9) {
+        return degree9(sumStr, degree + 1);
+    }
+    else{
+        return degree;
+    }
 }
 
 int main() {
-  char num[1001];
-  while (1) {
-    scanf("%s", num);
-    if (num[0] == '0' &&strlen(num)==1) {
-        break;
-    }
-    int degree = degree9(num);
-
-        if (degree > 0) {
-            printf("%s is a multiple of 9 and has 9-degree %d.\n", num, degree);
-        } else {
-            printf("%s is not a multiple of 9.\n", num);
+    char num[1001];
+    while (1) {
+        scanf("%s", num);
+        if (num[0] == '0' && strlen(num) == 1) {
+            break;
         }
-    
-  }
-  return 0;
+        int deg = degree9(num, 1);
+
+        if (deg == -1) {
+            printf("%s is not a multiple of 9.\n", num);
+        } else {
+            printf("%s is a multiple of 9 and has 9-degree %d.\n", num, deg);
+        }
+    }
+    return 0;
 }
