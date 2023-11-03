6ebfa6b221eae3c1a2e5e64786a17b1d311c1768
HladczukLe <leticia.cassia.hr@gmail.com>
2023-11-03 20:55:42 -0300

    add exercicio R

---
+++ lista-2/remocao.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct celula {
+    int dado;
+    struct celula *prox;
+} celula;
+
+int remove_depois(celula *p) {
+    if (p == NULL || p->prox == NULL) {
+        // Não é possível remover o elemento seguinte a p
+        return 0;
+    }
+
+    celula *temp = p->prox;
+    p->prox = temp->prox;
+    free(temp);
+    return 1;
+}
+
+void remove_elemento(celula *le, int x) {
+    celula *atual = le;
+    while (atual->prox != NULL) {
+        if (atual->prox->dado == x) {
+            celula *temp = atual->prox;
+            atual->prox = temp->prox;
+            free(temp);
+            return;
+        }
+        atual = atual->prox;
+    }
+}
+
+void remove_todos_elementos(celula *le, int x) {
+    celula *atual = le;
+    while (atual->prox != NULL) {
+        if (atual->prox->dado == x) {
+            celula *temp = atual->prox;
+            atual->prox = temp->prox;
+            free(temp);
+        } else {
+            atual = atual->prox;
+        }
+    }
+}
