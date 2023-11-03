86e6e004460b0cb34040944b2293c1f7387cce2c
HladczukLe <leticia.cassia.hr@gmail.com>
2023-11-03 01:02:09 -0300

    fix exercicio O

--- lista-2/impressao.c
+++ lista-2/impressao.c
@@ -8,27 +8,27 @@ typedef struct celula {
 
 void imprime(celula *le){
     if(le==NULL){
-        printf("NULL");
+        printf("NULL\n");
     }
     else{
-        while(le!=NULL){
-            printf("%d ", le->dado);
+        while(le != NULL){
+            printf("%d", le->dado);
+            if (le->prox != NULL) {
+                printf(" -> ");
+            }
             le = le->prox;
         }
-        printf("NULL");
+        printf(" -> NULL\n");
     }
 }
 
-void imprime_rec(celula *le){
-    if(le==NULL){
-        printf("NULL");
-    }
-    else{
-        printf("%d ", le->dado);
+void imprime_rec(celula *le) {
+    if (le == NULL) {
+        printf("NULL\n");
+    } else {
+        printf("%d", le->dado);
         if (le->prox != NULL) {
             printf(" -> ");
-        } else {
-            printf("NULL");
         }
         imprime_rec(le->prox);
     }
