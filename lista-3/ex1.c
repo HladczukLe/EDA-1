77d9b706e9a1cb9ce10c742355a3df176576dbff
HladczukLe <leticia.cassia.hr@gmail.com>
2023-11-03 23:30:05 -0300

    add exercicio 1 - lista 3

---
+++ lista-3/ex1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+typedef struct fila {
+    int *dados;
+    int N, p, u; //N: tamanho da fila; p: O índice do ponteiro de início da fila; u: O índice do ponteiro de fim da fila
+} fila;
+
+int desenfileira (fila *f, int *y){
+    if(f->p == f->u){
+        //verifica se a lista está vazia
+        return 0;
+    }
+
+    //remove o elemento da fila e salva em y
+    *y = f->dados[f->p];
+
+    //atualiza p para o próximo elemento
+    f->p = (f->p + 1) % f->N;
+
+    return 1;
+}
